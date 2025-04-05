131a4c489802e8ae4b4ceeca95a1367eb98e224d
Hydenaky <juanpi.cn2004@gmail.com>
2025-04-04 19:48:26 -0500

    form changes

--- src/app/clients/components/form/form.component.ts
+++ src/app/clients/components/form/form.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface City {
+  name: string;
+  code: string;
+}
+
 @Component({
   selector: 'app-form',
   standalone: false,
@@ -7,5 +12,19 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  text1: string | undefined;
 
+  text2: string | undefined;
+
+  number: string | undefined;
+
+  selectedCity: City | undefined;
+
+  cities: City[] = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' },
+  ];
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,9 +3,23 @@ import { provideAnimationsAsync } from "@angular/platform-browser/animations/asy
 import { providePrimeNG } from "primeng/config";
 import Aura from '@primeng/themes/aura';
 import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 const MaterialComponents=[
-    ButtonModule
+    ButtonModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    FloatLabelModule,
+    FormsModule,
+    InputTextModule,
+    SelectModule,
+    InputNumberModule
 ]
 @NgModule({
 
